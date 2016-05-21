4892e1be8d1899bd089f62be7e2a6cb791df13c8
Ty Kennington <tyjkenn@gmail.com>
2016-05-21 17:51:29 -0600

    Displays favorite scripture

--- app/src/main/java/com/pointnote/scripturereference/ShareActivity.java
+++ app/src/main/java/com/pointnote/scripturereference/ShareActivity.java
@@ -1,7 +1,9 @@
 package com.pointnote.scripturereference;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ShareActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class ShareActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_share);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        TextView view = (TextView) findViewById(R.id.message);
+        view.setText(message);
     }
 }
